faf175bb6bac322dcb9cf114993f1362c0891495
Miroslav Horvatov <52826419+Kimleta@users.noreply.github.com>
2022-12-26 15:52:39 +0100

    Update gymsoftware.sql

--- gymsoftware.sql
+++ gymsoftware.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Dec 25, 2022 at 08:34 PM
+-- Generation Time: Dec 26, 2022 at 02:51 PM
 -- Server version: 5.7.36
--- PHP Version: 8.0.13
+-- PHP Version: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,19 +30,24 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `admin_user`;
 CREATE TABLE IF NOT EXISTS `admin_user` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `email` text COLLATE utf8mb4_bin NOT NULL,
   `username` varchar(100) COLLATE utf8mb4_bin NOT NULL,
   `password` varchar(100) COLLATE utf8mb4_bin NOT NULL,
   `plan` text COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`ID`),
   UNIQUE KEY `ID` (`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `admin_user`
 --
 
-INSERT INTO `admin_user` (`ID`, `username`, `password`, `plan`) VALUES
-(1, 'admin', 'admin123', 'Pro');
+INSERT INTO `admin_user` (`ID`, `email`, `username`, `password`, `plan`) VALUES
+(1, 'test@test.com', 'admin', 'admin123', 'Pro'),
+(3, 'asddsadas@test.xyz', 'ad', 'ad', 'Free'),
+(4, 'asddsadas@test.xyz1', 'admin2', 'ad2', 'Free'),
+(5, 'asddsadas@test.xyz11', 'admin22', '12345', 'Free'),
+(6, 'dasdasd@test.fs', 'ads', 'asdf', 'Free');
 
 -- --------------------------------------------------------
 
@@ -68,6 +73,7 @@ DROP TABLE IF EXISTS `gym`;
 CREATE TABLE IF NOT EXISTS `gym` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `Name` varchar(300) COLLATE utf8mb4_bin NOT NULL,
+  `Admin ID` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
