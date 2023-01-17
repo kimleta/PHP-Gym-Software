e4638516adab36e99e6277069c42d49f18734a22
Miroslav Horvatov <52826419+Kimleta@users.noreply.github.com>
2023-01-17 09:44:24 +0100

    Update packagesLogic.php

--- config/packagesLogic.php
+++ config/packagesLogic.php
@@ -41,6 +41,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             }
         }
 
+        // Check if there 
+        /// inserting into DB
         $sqlPackage = "INSERT INTO `package_info`( `Gym ID`, `Name`, `descripction`) VALUES ('$gymID','$name','$description')";
         $result = mysqli_query($con, $sql);
     }
